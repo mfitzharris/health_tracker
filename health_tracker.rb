63baab6da4168889485826d225a40cedbaa85d4e
mfitzharris <molly.fitzharris@gmail.com>
2016-09-01 17:47:52 -0500

    Write ruby code to create database, and tables in database

--- health_tracker.rb
+++ health_tracker.rb
@@ -7,33 +7,73 @@
 # dont use classes
 
 ### Simple Health Tracker ###
+### database ###
 # set up database w columns for tracking:
-  # the date
-  # general physical status on scale 1-10
-  # general mental status on scale 1-10
-  # physical ailments (foreign key)
-    # ailment + comments
-    # headache 
-    # indigestion 
-    # fatigue 
-    # body aches
-    # other 
-    # none + none
-  # physical activity (steps walked/ran)
-# UI  
+    # id
+    # the date
+    # general physical status on scale 1-10
+    # general mental status on scale 1-10
+    # physical activity (steps walked/ran)
+    # comment on physical ailments
+    # ailments (foreign key)
+      # ailment + comments
+        # headache 
+        # indigestion/stomach ache 
+        # fatigue 
+        # body aches
+        # flu or flu-like symptoms
+        # cold or cold-like symptoms
+        # depression/anxiety
+        # other 
+        # none
+    
+### UI ### 
 # let the user to enter the data
-  # "what is the date today"
-    # or is there a way to automatically get today's date?
-  # "How are you feeling (physically) on a scale of 1-10"
-  # "How are you feeling (mentally) on a scale of 1-10"
-  # "Any physical ailments today?"
-    # list them for user to choose from
-    # or if/else structure ??? 
-    # or??????
-  # "How many steps did you walk/run today?"
+    # "what is the date today"
+      # or is there a way to automatically get today's date?
+    # "How are you feeling (physically) on a scale of 1-10"
+    # "How are you feeling (mentally) on a scale of 1-10"
+    # "Any physical ailments today?"
+      # list them for user to choose from
+      # or if/else structure ??? 
+      # or??????
+    # "How many steps did you walk/run today?"
 # let the user access the data
-  # run down of the week/month/year? --> 
-    # averages
-    # % of days w ailments ()
-  # print out a day's health data (any day/day entered)
+    # run down of the week/month/year? --> 
+      # averages
+      # % of days w ailments ()
+    # print out a day's health data (any day/day entered)
+
+require 'sqlite3'
+
+db = SQLite3::Database.new("health.db")
+db.results_as_hash = true # do i want this??? play w later
+
+ailment_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS ailments (
+    id INTEGER PRIMARY KEY,
+    ailment VARCHAR (255)
+  )
+SQL
+
+health_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS health (
+  id INTEGER PRIMARY KEY,
+  dt datetime default current_timestamp,
+  phys_stat INT,
+  ment_stat INT,
+  steps INT,
+  ailment_cmt VARCHAR (255),
+  ailment INT,
+  FOREIGN KEY (ailment) REFERENCES ailments (id)
+  )
+SQL
+
+db.execute(ailment_table_cmd)
+db.execute(health_table_cmd)
+
+
+
+
+
 
