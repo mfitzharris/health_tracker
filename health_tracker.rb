ba62f48d6a93809cadfee4a670164f3d906060cd
mfitzharris <molly.fitzharris@gmail.com>
2016-09-01 16:38:44 -0500

    Pseudocode health tracker program

---
+++ health_tracker.rb
@@ -0,0 +1,39 @@
+### CHALLENGE DIRECTIONS ###
+# program w persistent data to make life better
+# find several ops to try something NOT shown in video
+  # can users create data?
+  # retrieve it?
+  # manipulate it?
+# dont use classes
+
+### Simple Health Tracker ###
+# set up database w columns for tracking:
+  # the date
+  # general physical status on scale 1-10
+  # general mental status on scale 1-10
+  # physical ailments (foreign key)
+    # ailment + comments
+    # headache 
+    # indigestion 
+    # fatigue 
+    # body aches
+    # other 
+    # none + none
+  # physical activity (steps walked/ran)
+# UI  
+# let the user to enter the data
+  # "what is the date today"
+    # or is there a way to automatically get today's date?
+  # "How are you feeling (physically) on a scale of 1-10"
+  # "How are you feeling (mentally) on a scale of 1-10"
+  # "Any physical ailments today?"
+    # list them for user to choose from
+    # or if/else structure ??? 
+    # or??????
+  # "How many steps did you walk/run today?"
+# let the user access the data
+  # run down of the week/month/year? --> 
+    # averages
+    # % of days w ailments ()
+  # print out a day's health data (any day/day entered)
+
